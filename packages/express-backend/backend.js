7527170f7161fc1f91c987923894a8153debc4ed
nathankang413 <nathankang413@gmail.com>
2024-01-16 14:31:41 -0800

    address users by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -37,7 +37,13 @@ const findUserByName = (name) => {
     return users["users_list"].filter(
         (user) => user.name === name
     );
-}
+};
+
+const findUserById = (id) => {
+    return users["users_list"].find(
+        (user) => user.id === id
+    );
+};
 
 // -- Endpoints -- //
 app.use(express.json());
@@ -55,7 +61,17 @@ app.get("/users", (req, res) => {
     } else {
         res.send(users);
     }
-})
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
+});
 
 app.listen(port, () => {
     console.log(

5dcb010d1e9b8117cdb96a31eae3edc8492857e4
nathankang413 <nathankang413@gmail.com>
2024-01-23 11:33:31 -0800

    connect backend with http requests

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,19 +1,61 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
 	const [characters, setCharacters] = useState([]);
 
+	function fetchUsers() {
+		const promise = fetch("http://localhost:8000/users");
+		return promise;
+	}
+
+	useEffect(() => {
+		fetchUsers()
+			.then((res) => res.json())
+			.then((json) => setCharacters(json["users_list"]))
+			.catch((error) => { console.log(error); })
+	}, [])
+
+	function deleteUser(person) {
+		const promise = fetch(`http://localhost:8000/users/${person.id}`, {
+			method: "DELETE"
+		})
+
+		return promise;
+	}
+
+	function postUser(person) {
+		const promise = fetch("http://localhost:8000/users", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(person),
+		})
+
+		return promise
+	}
+
 	function removeOneCharacter(index) {
-		const updated = characters.filter((character, i) => {
-			return i !== index;
-		});
-		setCharacters(updated);
+		deleteUser(characters[index])
+			.then((res) => {
+				if (res.status === 204) {
+					setCharacters(characters.filter((character, i) => {
+						return i !== index;
+					}))
+				}
+			})
+			.catch((error) => { console.log(error) })
 	}
 
 	function updateList(person) {
-		setCharacters([...characters, person])
+		// only adds the person locally if the POST resolves
+		postUser(person)
+			.then((res) => res.status === 201 ? res : undefined)
+			.then((res) => res.json())
+			.then((json) => setCharacters([...characters, json.user]))
+			.catch((error) => { console.log(error) })
 	}
 
 	return (
